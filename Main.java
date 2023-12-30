f9a1b8046b6caa8ebe58c84c86d7fe2b9feed734
slava <slava.cherkasov.1997@list.ru>
2023-12-30 22:06:06 +0700

    pa

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
 package sem1;
 import java.util.*;
 
+import units.Archer;
+import units.Coords;
+import units.Hero;
+import units.Monk;
+import units.Names;
+import units.Rober;
+import units.Sniper;
+import units.Sorcerer;
+import units.Villager;
+import units.Wizard;
+
 public class Main {
     
     public static void main(String[] args) {
       
-        List<Hero> whiteteam = FillArray();
-        List<Hero> blackteam = FillArray();
+        List<Hero> whiteteam = getTeam(10,"white");
+        List<Hero> blackteam = getTeam(10,"black");
 
         System.out.println("team white\n");
         for (Hero hero : whiteteam) {
@@ -24,44 +35,36 @@ public class Main {
      
     }
 
-    static  List<Hero> FillArray()
+    static  List<Hero> FillArray(int num)
     {   List<Hero> gameHeroeList = new ArrayList<>();
         Random rand  = new Random();
-        Hero archer = new Archer(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero monk = new Monk(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero rober = new Rober(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero sniper = new Sniper(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero sorcerer = new Sorcerer(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero wizard = new Wizard(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
-        Hero villager = new Villager(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString());
         
-      
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < num; i++) {
             
             int number = rand.nextInt(0,7);
 
             switch (number) {
                 case 0:
-                    gameHeroeList.add(archer);
+                    gameHeroeList.add(new Archer(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 case 1:
-                    gameHeroeList.add(monk);
+                    gameHeroeList.add(new Monk(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 case 2:
-                    gameHeroeList.add(rober);
+                    gameHeroeList.add(new Rober(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 case 3:
-                    gameHeroeList.add(sniper);
+                    gameHeroeList.add(new Sniper(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
 
                 case 4:
-                    gameHeroeList.add(sorcerer);
+                    gameHeroeList.add( new Sorcerer(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 case 5:
-                    gameHeroeList.add(wizard);
+                    gameHeroeList.add(new Wizard(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 case 6:
-                    gameHeroeList.add(villager);
+                    gameHeroeList.add( new Villager(Names.values()[new Random().nextInt(0,Names.values().length-1)].toString()));
                     break;
                 default:
                     break;
@@ -71,6 +74,30 @@ public class Main {
 
     return gameHeroeList;
 
+   }
+
+
+   static List<Hero> getTeam(int number,String mod)
+   {     List<Hero> list = FillArray( number);
+        List <Hero> team = new ArrayList<>(number);
+        if (mod == "white") {
+            for (int i = 0; i < list.size(); i++) {
+                team.add(list.get(i));
+                team.get(i).x = 0;
+                team.get(i).y = i;
+                team.get(i).white = true;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < list.size(); i++) {
+                team.add(list.get(i));
+                team.get(i).x = Coords.max_x;
+                team.get(i).y = i;
+                 team.get(i).white = false;
+            }
+        }
+        return team;
    }
 
 }
